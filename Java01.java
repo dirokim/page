402167b2ee28448eed0634c82c74b7c00896c1d6
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2023-12-04 21:06:16 +0900

    Add files via upload
    개발 일지 첫자바 !

---
+++ Java01.java
@@ -0,0 +1,23 @@
+
+public class Java01 {
+	public static void main(String[]args) {
+	System.out.println("Hello World");
+	//주석 
+	//변수 변하는값 
+	//상수:변하지 않는값 
+	//리터럴 : 변수에 대입하는 데이터
+	int num = 4;
+	long num1 = 10;
+		num = num + (int)num1;
+	System.out.println(num);
+	//int 정수 
+	//double 실수
+	//char 문자 ''
+	//boolean true or false
+	//string 문자열 ""
+	//연산자 +,-,*,/,%
+	// = 대입연산자 우측값 좌측에 대입
+	//삼항 연산자 String sum = height > 180 ? "큰키":"낮은키"	
+		
+	}
+}
