814e8c7826cc6d98bdc07ff12d7878a44e5a514f
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2023-12-07 21:42:00 +0900

    basic

---
+++ Java02.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class Java02 {
+	public static void main(Stirng[]args) {
+		
+		
+		//class  = object 객체 자바는 객체 지향 언어
+		//멤버 변수선언 = 객체선언
+		//배열선언 Student [] 변수명 = new Student[배열length]
+		//객체 변수 Student stu = new Student();
+		//권한지정자 4가지 중 하나 선택
+		//public, 어디서든 사용가능 제한없음
+		//protected, 같은패키지내에서만 가능 상속관계에서는 다른패키지도 가능
+		//default,  같은패키지내 자바내에서는 생략 
+		//private , 같은클래스내에서만 사용 가능 
+		//캡슐화 외부에서 접근하는데 정보를은닉 메서드를 통해서만 접근 =>데이터보호
+	}
+}
