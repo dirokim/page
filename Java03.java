d74a663828eef147eeed06a2f837190f8c44c1d8
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2023-12-08 22:51:27 +0900

    Add files via upload

---
+++ Java03.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Java03 {
+	public static void main(String[]args) {
+		
+		// 상속 inherit ,extends 
+		//상위 클래스가 하위클래스에게 상속
+		//super class > child class 
+		//재사용성 public class 클래스명 extends 클래스명 {}
+		//public class SuperClass{}
+		//overloading ,매개변수 찾아가고 /  overiding 상위클래스에 타다  
+	}
+}
