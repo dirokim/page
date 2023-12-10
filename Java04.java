6beb57740c2c4c9f43882e1c83c89e170037cb15
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2023-12-10 23:30:55 +0900

    Add files via upload

---
+++ Java04.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Java04 {
+	public static void main(String[]args) {
+		
+		//Package 클래스 내 다른클래스 같은패키지에서 찾음
+		//메서드 charAt(int index) 문자열에서 해당인덱스 번호의문자 (char)를반환
+		//indexOf(int ch) 문자열의 인덱스번호를 반환 
+		//length() 문자열의 길이 반환
+		
+	}
+}
